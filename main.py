7bc46c1842157670331d3b8c61991ab9c2ee334b
Daniil Melnikov <dnl.melnikov@yandex.ru>
2021-09-13 17:42:38 +0700

    student put grade

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-
+    def put_grade(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 class Mentor:
     def __init__(self, name, surname):
@@ -14,7 +21,13 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
-    def rate_hw(self, student, course, grade):
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+class Reviewer(Mentor):
+    def put_grade(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
@@ -23,19 +36,19 @@ class Mentor:
         else:
             return 'Ошибка'
 
-class Lecturer(Mentor):
-    pass
-class Reviewer(Mentor):
-    pass
+cool_lecturer = Lecturer('Som', 'Bud')
+cool_lecturer.courses_attached += ['Python']
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
+best_student.put_grade(cool_lecturer, 'Python', 10)
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer.put_grade(best_student, 'Python', 10)
+cool_reviewer.put_grade(best_student, 'Python', 10)
+cool_reviewer.put_grade(best_student, 'Python', 10)
 
 print(best_student.grades)
+print(cool_lecturer.grades)
