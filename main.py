c71d73fd913edfda2e504fe16579f87568b5bcd2
Daniil Melnikov <dnl.melnikov@yandex.ru>
2021-09-14 10:48:49 +0700

    add _lt_ lecturer

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Student:
         self.grades = {}
 
     def put_grade(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached:
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
@@ -73,6 +73,26 @@ class Lecturer(Mentor):
             f'Средняя оценка за лекции: {round(result, 1)} \n'
         return res
 
+    def __lt__(self, other):
+        sum_len = 0
+        sum_grade = 0
+        for value in self.grades.values():
+            for grade in value:
+                sum_len += 1
+                sum_grade += grade
+        self.result = sum_grade / sum_len
+        sum_len = 0
+        sum_grade = 0
+        for value in other.grades.values():
+            for grade in value:
+                sum_len += 1
+                sum_grade += grade
+        other.result = sum_grade / sum_len
+        if not isinstance(other, Lecturer):
+            print('Не лектор!')
+            return
+        return self.result < other.result
+
 class Reviewer(Mentor):
     def put_grade(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -96,12 +116,12 @@ GIT_lecturer.courses_attached += ['GIT']
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python', 'GIT']
 best_student.put_grade(Python_lecturer, 'Python', 10)
-best_student.put_grade(Python_lecturer, 'Python', 9)
+best_student.put_grade(GIT_lecturer, 'GIT', 9)
 
 other_student = Student('Serkan', 'Boulat', 'man')
 other_student.courses_in_progress += ['Python', 'GIT']
-other_student.put_grade(GIT_lecturer, 'GIT', 9)
-other_student.put_grade(GIT_lecturer, 'GIT', 10)
+other_student.put_grade(Python_lecturer, 'Python', 8)
+other_student.put_grade(GIT_lecturer, 'GIT', 1)
 
 cool_reviewer = Reviewer('Some', 'Buddy')
 cool_reviewer.courses_attached += ['Python']
@@ -115,9 +135,9 @@ cool_reviewer.put_grade(other_student, 'Python', 10)
 cool_reviewer.put_grade(other_student, 'GIT', 9)
 cool_reviewer.put_grade(other_student, 'GIT', 9)
 
-
 print(best_student > other_student)
-
+print(GIT_lecturer < Python_lecturer)
 print(cool_reviewer)
 print(Python_lecturer)
+print(GIT_lecturer)
 print(best_student)
